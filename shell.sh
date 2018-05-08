8cc1f56ebc8a9777e7628cdf95da814ddfe5a987
Villarreal Pertuz <jvillarrealpertuz@uslmajvillarrmb.us.deloitte.com>
2018-05-08 12:19:44 -0400

    updated ssm script

--- shell.sh
+++ shell.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "It's about to start"
-cd /tmp
+mkdir /tmp/ssm
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+sudo systemctl enable amazon-ssm-agent
 sudo systemctl start amazon-ssm-agent
 
